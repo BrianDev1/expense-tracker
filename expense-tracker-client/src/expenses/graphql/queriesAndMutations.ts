ba902712d5c199bc1831f7d44d021f7759fed4a9
Brian Rudolph <brian.rudolph303@gmail.com>
2021-09-24 05:56:16 -0600

    fix: Comments

--- expense-tracker-client/src/expenses/graphql/queriesAndMutations.ts
+++ expense-tracker-client/src/expenses/graphql/queriesAndMutations.ts
@@ -1,7 +1,7 @@
 import { gql } from "@apollo/client";
 import { Expense } from "../utils/types";
 
-// Fragment for code reusability
+// Graphql Fragment for code reusability
 export const ExpenseFragment = gql`
   fragment ExpenseFields on Expense {
     id
@@ -21,12 +21,7 @@ export const fetchExpenses = gql`
   ${ExpenseFragment}
 `;
 
-export interface IFetchExpensesData {
-  readonly fetchExpenses: Expense[];
-}
-
 /* MUTATIONS */
-// CREATE
 export const createExpense = gql`
   mutation createExpense($inputCreateExpense: InputCreateExpense!) {
     createExpense(inputCreateExpense: $inputCreateExpense) {
@@ -36,11 +31,6 @@ export const createExpense = gql`
   ${ExpenseFragment}
 `;
 
-export interface ICreateExpense {
-  readonly createExpense: Expense;
-}
-
-// UPDATE
 export const updateExpense = gql`
   mutation updateExpense($inputUpdateExpense: InputUpdateExpense!) {
     updateExpense(inputUpdateExpense: $inputUpdateExpense) {
@@ -50,16 +40,25 @@ export const updateExpense = gql`
   ${ExpenseFragment}
 `;
 
-export interface IUpdateExpense {
-  readonly updateExpense: Expense;
-}
-
-// DELETE
 export const deleteExpense = gql`
   mutation deleteExpense($id: String!) {
     deleteExpense(id: $id)
   }
 `;
+
+/* Query / Mutation return interfaces */
+
+export interface IFetchExpensesData {
+  readonly fetchExpenses: Expense[];
+}
+
+export interface ICreateExpense {
+  readonly createExpense: Expense;
+}
+
+export interface IUpdateExpense {
+  readonly updateExpense: Expense;
+}
 
 export interface IDeleteExpense {
   readonly deleteExpense: string;
